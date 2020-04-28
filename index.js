c7290136c72f81c62f29f1c3d3242e4c710bd140
Mohammed-Elsamman <monem7242@gmail.com>
2020-04-29 01:35:14 +0200

    deploye to heroku

--- index.js
+++ index.js
@@ -37,7 +37,13 @@ const insertDummyTables = async () => {
     });
 };
 insertDummyTables().then();
-
+//server static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build'))
+    })
+}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
